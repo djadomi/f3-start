fa5261d6c114853cf0ce41d53ac478870fd72e7b
dom <dja@domi.co.uk>
2021-01-29 10:07:32 +0100

    add a couple of standard functions

--- htdocs/f3/classes/controller/main.php
+++ htdocs/f3/classes/controller/main.php
@@ -14,4 +14,17 @@ class Main extends \Prefab {
 	function page() {
 		$f3 = \Base::instance();
 	}
+	function error() {
+		$f3 = \Base::instance();
+		$f3->result = \json_encode([
+			'error' => true,
+			'code' => $f3->get('ERROR.code'),
+			'status' => $f3->get('ERROR.status'),
+			'text' => $f3->get('ERROR.text'),
+		]);
+		$this->log->write("ERROR: Main->error: " . $this->jsonlog(json_decode($f3->result)), 0);
+	}
+	function jsonlog($i) {
+		return preg_replace('/[\s\t\n]+/', ' ', json_encode($i, JSON_PRETTY_PRINT));
+	}
 }
