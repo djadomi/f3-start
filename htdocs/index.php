ed3e842167f4b0c41a39c19a1815317b52c11ebe
dom <dja@domi.co.uk>
2025-02-14 12:05:39 +0000

    fix paths

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,7 +1,7 @@
 <?php
-require_once '/www/composer/vendor/autoload.php';
+require_once '../vendor/autoload.php';
 $f3 = \Base::instance();
-$f3->config('f3/config/config.ini', true);
-$f3->config("f3/config/{$f3->get('HOST')}.ini", true);
-$f3->config('f3/config/routes.ini');
+$f3->config('../config/config.ini', true);
+$f3->config("../config/{$f3->get('HOST')}.ini", true);
+$f3->config('../config/routes.ini');
 $f3->run();
