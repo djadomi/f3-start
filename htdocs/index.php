f220a341f7f8c16030530f887de5c709fe172400
dom <dja@domi.co.uk>
2025-02-14 12:56:58 +0000

    extra config loaded by config.ini, not index.php

--- htdocs/index.php
+++ htdocs/index.php
@@ -2,6 +2,4 @@
 require_once '../vendor/autoload.php';
 $f3 = \Base::instance();
 $f3->config('../config/config.ini', true);
-$f3->config("../config/{$f3->get('HOST')}.ini", true);
-$f3->config('../config/routes.ini');
 $f3->run();
